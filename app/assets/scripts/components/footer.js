1865c93ee642738a853fd4de73dcb2fca2713150
Ricardo Mestre <ricardo@duplos.org>
2017-02-28 18:40:58 +0000

    Include footer nav

--- app/assets/scripts/components/footer.js
+++ app/assets/scripts/components/footer.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import config from '../config'
+import { Link, IndexLink } from 'react-router';
 
 const Footer = React.createClass({
 
@@ -9,10 +9,16 @@ const Footer = React.createClass({
     return (
       <footer className='page__footer' role='contentinfo'>
         <div className='inner'>
-          <p>
-            Rural Road Accessibility
-            <small>2017 © World Bank Group</small>
-          </p>
+          <nav className='page__footer-nav' role='navigation'>
+            <ul className='nav-menu'>
+              <li><IndexLink to='/' title='Visit projects page'><span>Projects</span></IndexLink></li>
+              <li><Link to='/about' title='Visit about page'><span>About</span></Link></li>
+              <li><Link to='/help' title='Visit help page'><span>Help</span></Link></li>
+            </ul>
+          </nav>
+          <div className='footer-credits'>
+            <p>Rural Road Accessibility <small>2017 © <a href='http://www.worldbank.org/' title='Visit website'>World Bank Group</a></small></p>
+          </div>
         </div>
       </footer>
     )
