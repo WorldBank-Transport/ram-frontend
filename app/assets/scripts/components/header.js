13a3b4a5e7b530f2d0854cf3aba7e81a0172593e
Daniel da Silva <daniel@developmentseed.org>
2017-03-02 11:38:02 +0000

    Add language switcher dropdown

--- app/assets/scripts/components/header.js
+++ app/assets/scripts/components/header.js
@@ -1,10 +1,12 @@
 'use strict';
 import React from 'react';
 import { Link } from 'react-router';
+import c from 'classnames';
 
-import { getLanguage } from '../utils/i18n';
+import { t, getLanguage, getLanguageName, getAvailableLanguages } from '../utils/i18n';
 
 import NavGlobalMenu from '../components/nav-global-menu';
+import Dropdown from '../components/dropdown';
 
 const Header = React.createClass({
 
@@ -18,16 +20,31 @@ const Header = React.createClass({
             <Link to={`/${getLanguage()}/`} title='Visit page'><h1 className='page__title'>Rural Road Accessibility</h1></Link>
           </div>
           <nav className='page__prime-nav' role='navigation'>
-            <div className='nav-language-switcher drop drop--down drop--align-left'>
-              <a href='#lang-switcher' title='Change language' className='drop__toggle drop__toggle--caret'><span>English</span></a>
-              <div className='drop__content' id='lang-switcher'>
-                <h6 className='drop__title'>Change language</h6>
-                <ul className='drop__menu drop__menu--select' role='menu'>
-                  <li><a href='#' title='Select language' className='drop__menu-item drop__menu-item--active'>English</a></li>
-                  <li><a href='#' title='Select language' className='drop__menu-item'>Language 2</a></li>
-                </ul>
-              </div>
-            </div>
+
+        <div className='nav-language-switcher'>
+          <Dropdown
+            id='lang-switcher'
+            triggerClassName='drop__toggle--caret'
+            triggerText={getLanguageName()}
+            triggerTitle={t('Change language')}
+            triggerElement='a'
+            direction='down'
+            alignment='left' >
+              <h6 className='drop__title'>{t('Change language')}</h6>
+              <ul className='drop__menu drop__menu--select' role='menu'>
+                {getAvailableLanguages().map(l => {
+                  let cl = c('drop__menu-item', {
+                    'drop__menu-item--active': l.key === getLanguage()
+                  });
+                  return (
+                    <li key={l.key}>
+                      <Link to={`/${l.key}`} title={t('Select language')} className={cl} data-hook='dropdown:close'>{l.name}</Link>
+                    </li>
+                  );
+                })}
+              </ul>
+          </Dropdown>
+        </div>
 
             <NavGlobalMenu />
           </nav>
