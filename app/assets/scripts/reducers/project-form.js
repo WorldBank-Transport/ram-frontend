ee57aaa97f3ab270d7756c5e8257f819c0d5fcaf
Daniel da Silva <daniel@developmentseed.org>
2017-03-03 12:15:23 +0000

    Add project delete action. Extract actions into component

--- app/assets/scripts/reducers/project-form.js
+++ app/assets/scripts/reducers/project-form.js
@@ -1,7 +1,14 @@
-import { RESET_PROJECT_FORM, START_SUBMIT_PROJECT, FINISH_SUBMIT_PROJECT } from '../actions';
+import {
+  RESET_PROJECT_FORM,
+  START_SUBMIT_PROJECT,
+  FINISH_SUBMIT_PROJECT,
+  START_DELETE_PROJECT,
+  FINISH_DELETE_PROJECT
+} from '../actions';
 
 const initialState = {
   processing: false,
+  action: null,
   error: null,
   data: {}
 };
@@ -11,8 +18,18 @@ export default function reducer (state = initialState, action) {
     case RESET_PROJECT_FORM:
       return Object.assign({}, state, initialState);
     case START_SUBMIT_PROJECT:
-      return Object.assign({}, state, {processing: true, error: null});
+      return Object.assign({}, state, initialState, {processing: true, action: 'edit'});
     case FINISH_SUBMIT_PROJECT:
+      state = Object.assign({}, state, { processing: false });
+      if (action.error) {
+        state.error = action.error;
+      } else {
+        state.data = action.data.results || action.data;
+      }
+      break;
+    case START_DELETE_PROJECT:
+      return Object.assign({}, state, initialState, {processing: true, action: 'delete'});
+    case FINISH_DELETE_PROJECT:
       state = Object.assign({}, state, { processing: false });
       if (action.error) {
         state.error = action.error;
