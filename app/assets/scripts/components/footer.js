3a6f79d2c9b575307574d5214e5cf96c1f63d48d
Ricardo Mestre <ricardo@duplos.org>
2017-05-03 14:13:57 +0100

    Remove credits translation string

--- app/assets/scripts/components/footer.js
+++ app/assets/scripts/components/footer.js
@@ -16,7 +16,7 @@ const Footer = React.createClass({
             <NavGlobalMenu />
           </nav>
           <div className='footer-credits'>
-            <p>{t('Rural Road Accessibility')} <small>2017 © <a href='http://www.worldbank.org/' title={t('Visit website')}>{t('The World Bank Group')}</a></small></p>
+            <p><strong>Rural Road Accessibility</strong> <small>2017 © <a href='http://www.worldbank.org/' title={t('Visit website')}>{t('The World Bank Group')}</a></small></p>
           </div>
         </div>
       </footer>
