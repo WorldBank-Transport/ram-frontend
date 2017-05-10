ce7c0497aa8680a9f55851b1b780bb5910f00ad7
Daniel da Silva <daniel@developmentseed.org>
2017-05-10 16:19:02 +0100

    sd

--- .build_scripts/deploy.sh
+++ .build_scripts/deploy.sh
@@ -1,27 +1,6 @@
 #!/usr/bin/env bash
 set -e # halt script on error
 
-# Setting correct variables based on the environment we're deploying to
-if [[ $TRAVIS_BRANCH == ${STAGING_BRANCH} ]]; then
-  LATEST_TAG=latest-dev
-elif [[ $TRAVIS_BRANCH == ${DEPLOY_BRANCH} ]]; then
-  LATEST_TAG=latest-stable
-fi
-elif [[ $TRAVIS_BRANCH == test ]]; then
-  echo $DOCKER_USERNAME
-  echo $DOCKER_PASSWD
-  exit 1;
-fi
-
-echo "Building source image"
-docker build -t $DOCKER_SRC_IMAGE .
-
-docker login -u="$DOCKER_USERNAME" -p="$DOCKER_PASSWD"
-
-echo "Pushing image to Docker Hub:$TRAVIS_COMMIT"
-docker tag $DOCKER_SRC_IMAGE $DOCKER_REPOSITORY:$TRAVIS_COMMIT
-docker push $DOCKER_REPOSITORY:$TRAVIS_COMMIT
-
-echo "Also pushing as :$LATEST_TAG"
-docker tag $DOCKER_SRC_IMAGE $DOCKER_REPOSITORY:$LATEST_TAG
-docker push $DOCKER_REPOSITORY:$LATEST_TAG
+echo $DOCKER_USERNAME
+echo $DOCKER_PASSWD
+exit 1;
