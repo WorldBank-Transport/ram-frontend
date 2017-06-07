25bb4fce73336be727569aa45c5bbd79437a4e41
Daniel da Silva <daniel@developmentseed.org>
2017-06-07 14:41:11 +0100

    Fix scenario subtitle on project active page

--- app/assets/scripts/views/project-page-active.js
+++ app/assets/scripts/views/project-page-active.js
@@ -239,7 +239,9 @@ const ProjectPageActive = React.createClass({
 
     if (scenario.scen_create && scenario.scen_create.status === 'running') {
       scenarioSubtitle = t('Creating scenario');
-    } else if (scenario.data.res_gen_at !== 0) {
+
+    // Checking against the string 0, because normally it's a date string.
+    } else if (scenario.data.res_gen_at !== '0') {
       scenarioSubtitle = t('Analysis complete');
 
       if (scenario.data.rn_updated_at > scenario.data.res_gen_at) {
