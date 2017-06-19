a3f5a704b82022f82a321fb2054174fb05758ea8
Daniel da Silva <daniel@developmentseed.org>
2017-06-19 19:46:43 +0100

    Implement file upload for origins

--- app/assets/scripts/components/project/project-source.js
+++ app/assets/scripts/components/project/project-source.js
@@ -12,6 +12,7 @@ import ModalPoi from './source-modals/modal-poi';
 import ModalProfile from './source-modals/modal-profile';
 import ModalAdminBounds from './source-modals/modal-admin-bounds';
 import ModalRoadNetwork from './source-modals/modal-road-network';
+import ModalOrigins from './source-modals/modal-origins';
 
 class PorjectSourceData extends React.Component {
   constructor (props) {
@@ -133,81 +134,3 @@ PorjectSourceData.propTypes = {
 };
 
 export default PorjectSourceData;
-
-// Modal Components.
-// One per type. Forms are better handled on their own.
-
-// ////////////////////////////////////////////////////////////////////////// //
-// /////////////                  Origins                       ///////////// //
-// ////////////////////////////////////////////////////////////////////////// //
-
-class ModalOrigins extends ModalBase {
-  onSubmit () {
-    console.log('Submit');
-  }
-
-  renderSourceFile () {
-    let sourceData = this.props.sourceData;
-    let hasFile = !!sourceData.files.length;
-
-    if (!hasFile) {
-      return (
-        <div className='form__group'>
-          <label className='form__label' htmlFor='origins'>{t('Source')}</label>
-          <input type='file' id='origins' name='origins' className='form__control' placeholder={t('Select a administrative boundaries file')} />
-        </div>
-      );
-    }
-
-    let file = this.props.sourceData.files[0];
-
-    return (
-      <div>
-        <div className='form__group'>
-          <label className='form__label' htmlFor='origins'>{t('Source')}</label>
-          <div className='form__input-group'>
-            <input type='text' id='origins' name='origins' className='form__control' placeholder={file.name} readOnly />
-            <div className='form__input-addon'><button type='button' className='button button--danger-plain button--text-hidden' title={t('Remove file')}><i className='collecticon-trash-bin'></i><span>{t('Remove file')}</span></button></div>
-          </div>
-        </div>
-        {file.data.indicators.map((o, i) => (
-          <fieldset className='form__fieldset' key={o.key}>
-            <div className='form__inner-header'>
-              <div className='form__inner-headline'>
-                <legend className='form__legend'>{t('Attribute {idx}', {idx: i + 1})}</legend>
-              </div>
-              <div className='form__inner-actions'>
-                <button type='button' className='fia-trash' title='Delete fieldset'><span>Delete</span></button>
-              </div>
-            </div>
-
-            <div className='form__hascol form__hascol--2 disabled'>
-              <div className='form__group'>
-                <label className='form__label' htmlFor={`key-${i}`}>{t('Key')}</label>
-                <input type='text' id={`key-${i}`} name={`key-${i}`} className='form__control' placeholder={o.key} />
-              </div>
-              <div className='form__group'>
-                <label className='form__label' htmlFor={`label-${i}`}>{t('Label')}</label>
-                <input type='text' id={`label-${i}`} name={`label-${i}`} className='form__control' placeholder={o.label} />
-              </div>
-            </div>
-          </fieldset>
-        ))}
-      </div>
-    );
-  }
-
-  renderBody () {
-    return (
-      <ModalBody>
-        <form className='form'>
-          {this.renderSourceFile()}
-        </form>
-      </ModalBody>
-    );
-  }
-}
-
-ModalOrigins.propTypes = {
-  sourceData: T.object
-};
---
+++ app/assets/scripts/components/project/source-modals/modal-origins.js
@@ -0,0 +1,351 @@
+'use strict';
+import React, { PropTypes as T } from 'react';
+import _ from 'lodash';
+import c from 'classnames';
+
+import config from '../../../config';
+import { t } from '../../../utils/i18n';
+import { postFormdata, fetchJSON } from '../../../actions';
+import { showGlobalLoading, hideGlobalLoading } from '../../global-loading';
+
+import { ModalBody } from '../../modal';
+import ModalBase from './modal-base';
+
+class ModalOrigins extends ModalBase {
+  constructor (props) {
+    super(props);
+    this.initState(props);
+  }
+
+  componentWillReceiveProps (nextProps) {
+    if (!this.props.revealed && nextProps.revealed) {
+      this.initState(nextProps);
+    }
+  }
+
+  initState (props) {
+    let fileField;
+    if (props.sourceData.files.length) {
+      console.log('props.sourceData.files', props.sourceData.files);
+      fileField = props.sourceData.files[0];
+      fileField.indicators = fileField.data.indicators;
+      fileField.availableInd = fileField.data.availableInd;
+    } else {
+      fileField = this.getBaseFileField();
+    }
+
+    this.state = {
+      fileField,
+      fileToRemove: null
+    };
+  }
+
+  getBaseFileField () {
+    return {
+      file: null,
+      size: 0,
+      uploaded: 0,
+      indicators: [{
+        key: '',
+        label: ''
+      }],
+      availableInd: []
+    };
+  }
+
+  onFileSelected (id, event) {
+    let fileField = _.clone(this.state.fileField);
+
+    // Store file reference.
+    const file = event.target.files[0];
+    fileField.file = file;
+    fileField.size = file.size;
+    fileField.uploaded = 0;
+
+    showGlobalLoading();
+
+    // File contents.
+    readFileAsJSON(file)
+      .then(res => {
+        // Get the indicator common to every feature. Number indicators only.
+        let indicators = res.features.map(o => {
+          let numberKeys = [];
+          Object.keys(o.properties).forEach(k => {
+            if (!isNaN(parseInt(o.properties[k]))) {
+              numberKeys.push(k);
+            }
+          });
+          return numberKeys;
+        });
+        let intersect = indicators.shift();
+        indicators.every(o => {
+          intersect = intersect.filter(i => o.indexOf(i) !== -1);
+          return !!intersect.length;
+        });
+        indicators = intersect;
+
+        hideGlobalLoading();
+
+        if (!indicators.length) {
+          return this.props._showAlert('danger', <p>Invalid file selected: There are no available attributes to select</p>, true);
+        }
+
+        // Select the first available indicator.
+        fileField.indicators = [{ key: indicators[0], label: '' }];
+        // Store all the available indicators.
+        fileField.availableInd = indicators;
+
+        this.setState({ fileField });
+      })
+      .catch(() => {
+        hideGlobalLoading();
+        this.props._showAlert('danger', <p>Invalid file selected: Not valid geoJSON file</p>, true);
+      });
+  }
+
+  onFileRemove (id, event) {
+    this.setState({
+      fileField: this.getBaseFileField(),
+      fileToRemove: id
+    });
+  }
+
+  onIndicatorRemove (idx) {
+    let fileField = _.cloneDeep(this.state.fileField);
+    fileField.indicators.splice(idx, 1);
+    this.setState({ fileField });
+  }
+
+  onIndicatorKeySelect (idx, event) {
+    let fileField = _.cloneDeep(this.state.fileField);
+    fileField.indicators[idx].key = event.target.value;
+    this.setState({ fileField });
+  }
+
+  onIndicatorLabelChange (idx, event) {
+    let fileField = _.cloneDeep(this.state.fileField);
+    fileField.indicators[idx].label = event.target.value;
+    this.setState({ fileField });
+  }
+
+  addIndicatorField () {
+    let fileField = _.clone(this.state.fileField);
+    fileField.indicators = fileField.indicators.concat({
+      key: this.state.fileField.availableInd[0],
+      label: ''
+    });
+
+    this.setState({ fileField });
+  }
+
+  allowSubmit () {
+    if (this.state.fileToRemove && !this.state.fileField.file) {
+      return true;
+    }
+    // Are all attributes valid?
+    let validAttr = this.state.fileField.indicators.every(o => o.key !== '' && o.label !== '');
+    // Check for doubles.
+    let doubles = _(this.state.fileField.indicators)
+      .groupBy('key')
+      .values()
+      .some(o => o.length > 1);
+
+    return validAttr && !doubles;
+  }
+
+  onSubmit () {
+    showGlobalLoading();
+
+    // Delete action.
+    let deleteFilesPromiseFn = () => Promise.resolve();
+    if (this.state.fileToRemove) {
+      deleteFilesPromiseFn = () => {
+        return fetchJSON(`${config.api}/projects/${this.props.projectId}/files/${this.state.fileToRemove}`, {method: 'DELETE'})
+          .then(() => {
+            this.setState({fileToRemove: null});
+          })
+          .catch(err => {
+            this.props._showAlert('danger', <p>An error occurred while deleting file {this.state.fileField.name}: {err.message}</p>, true);
+            // Rethrow to stop chain.
+            throw err;
+          });
+      };
+    }
+
+    // Data to submit.
+    let newFilesPromiseFn = () => Promise.resolve();
+    if (this.state.fileField.file || this.state.fileField.created_at) {
+      newFilesPromiseFn = () => {
+        let formData = new FormData();
+        formData.append('source-type', 'file');
+        formData.append('source-name', 'origins');
+
+        this.state.fileField.availableInd.forEach(o => {
+          formData.append('available-ind', o);
+        });
+
+        // Submit keys and labels as arrays. The order is guaranteed.
+        this.state.fileField.indicators.forEach(o => {
+          formData.append('indicators[key]', o.key);
+          formData.append('indicators[label]', o.label);
+        });
+
+        if (this.state.fileField.file) {
+          formData.append('file', this.state.fileField.file);
+        }
+
+        let onProgress = progress => {
+          let fileField = _.clone(this.state.fileField);
+          fileField.uploaded = progress;
+          this.setState({fileField});
+        };
+
+        let { promise } = postFormdata(`${config.api}/projects/${this.props.projectId}/source-data`, formData, onProgress);
+        // this.xhr = xhr;
+        return promise
+          .then(res => {
+            let fileField = _.clone(this.state.fileField);
+            fileField = res;
+            fileField.indicators = res.data.indicators;
+            fileField.availableInd = res.data.availableInd;
+            this.setState({fileField});
+          })
+          .catch(err => {
+            this.props._showAlert('danger', <p>An error occurred while uploading a population file: {err.message}</p>, true);
+            // Rethrow to stop chain.
+            throw err;
+          });
+      };
+    }
+
+    deleteFilesPromiseFn()
+      .then(() => newFilesPromiseFn())
+      .then(res => {
+        this.setState({fileToRemove: null});
+        hideGlobalLoading();
+        this.props.onCloseClick(true);
+      })
+      .catch(e => {
+        hideGlobalLoading();
+      });
+  }
+
+  renderEmptyFileField () {
+    let { fileField } = this.state;
+
+    return (
+      <div className='form__group'>
+        <label className='form__label' htmlFor='profile'>{t('Source')}</label>
+        <input
+          type='file'
+          id='profile'
+          name='profile'
+          className='form__control'
+          placeholder={t('Select a profile file')}
+          onChange={this.onFileSelected.bind(this, fileField.id)}
+        />
+        {fileField.file !== null
+          ? <p className='form__help'>{Math.round(fileField.uploaded / (1024 * 1024))}MB / {Math.round(fileField.size / (1024 * 1024))}MB</p>
+          : null
+        }
+      </div>
+    );
+  }
+
+  renderFullFileField () {
+    let { fileField } = this.state;
+
+    return (
+      <div className='form__group'>
+        <label className='form__label' htmlFor='profile'>{t('Source')}</label>
+        <div className='form__input-group'>
+          <input type='text' id='profile' name='profile' className='form__control' placeholder={fileField.name} readOnly />
+          <div className='form__input-addon'>
+            <button
+              type='button'
+              className='button button--danger-plain button--text-hidden'
+              onClick={this.onFileRemove.bind(this, fileField.id)}
+              title={t('Remove file')}>
+              <i className='collecticon-trash-bin'></i><span>{t('Remove file')}</span>
+            </button>
+          </div>
+        </div>
+      </div>
+    );
+  }
+
+  renderIndicators () {
+    let { fileField } = this.state;
+
+    return fileField.indicators.map((o, i) => (
+      <fieldset className={c('form__fieldset', {disabled: fileField.file === null})} key={`${o.key}-${i}`}>
+        <div className='form__inner-header'>
+          <div className='form__inner-headline'>
+            <legend className='form__legend'>{t('Attribute {idx}', {idx: i + 1})}</legend>
+          </div>
+          <div className='form__inner-actions'>
+            <button type='button' className={c('fia-trash', {disabled: fileField.indicators.length <= 1})} title='Delete fieldset' onClick={this.onIndicatorRemove.bind(this, i)}><span>Delete</span></button>
+          </div>
+        </div>
+
+        <div className='form__hascol form__hascol--2'>
+          <div className='form__group'>
+            <label className='form__label' htmlFor={`key-${i}`}>{t('Key')}</label>
+            <select id={`key-${i}`} name={`key-${i}`} className='form__control' value={o.key} onChange={this.onIndicatorKeySelect.bind(this, i)}>
+              {fileField.availableInd.map(ind => <option key={ind} value={ind}>{ind}</option>)}
+            </select>
+          </div>
+
+          <div className='form__group'>
+            <label className='form__label' htmlFor={`label-${i}`}>{t('Label')}</label>
+            <input type='text' id={`label-${i}`} name={`label-${i}`} className='form__control' value={o.label} onChange={this.onIndicatorLabelChange.bind(this, i)} />
+          </div>
+        </div>
+      </fieldset>
+    ));
+  }
+
+  renderBody () {
+    let { fileField } = this.state;
+    let hasFile = !!fileField.created_at;
+
+    return (
+      <ModalBody>
+        <form className='form'>
+          {hasFile ? this.renderFullFileField() : this.renderEmptyFileField()}
+          {this.renderIndicators()}
+          <div className='form__extra-actions'>
+            <button type='button' className={c('fea-plus', {disabled: fileField.file === null})} title='Add new attribute' onClick={this.addIndicatorField.bind(this)}><span>New attribute</span></button>
+          </div>
+        </form>
+      </ModalBody>
+    );
+  }
+}
+
+ModalOrigins.propTypes = {
+  sourceData: T.object,
+  projectId: T.number,
+  _showAlert: T.func
+};
+
+export default ModalOrigins;
+
+function readFileAsJSON (file) {
+  return new Promise((resolve, reject) => {
+    let reader = new FileReader();
+
+    reader.onerror = err => reject(err);
+
+    reader.onload = e => {
+      try {
+        let json = JSON.parse(e.target.result);
+        return resolve(json);
+      } catch (err) {
+        return reject(err);
+      }
+    };
+
+    reader.readAsText(file);
+  });
+}
