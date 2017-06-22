b89b328650ea3f2dd26fd1ef42d7142d6b53e69e
Daniel da Silva <daniel@developmentseed.org>
2017-06-22 18:34:16 +0100

    Enable road network upload on scenario create

--- app/assets/scripts/components/scenario/scenario-create-modal.js
+++ app/assets/scripts/components/scenario/scenario-create-modal.js
@@ -269,9 +269,14 @@ const ScenarioCreateModal = React.createClass({
                 <span className='form__option__text'>{t('Clone from scenario')}</span>
                 <span className='form__option__ui'></span>
               </label>
-              <label className='form__option form__option--inline form__option--custom-radio disabled'>
+              <label className='form__option form__option--inline form__option--custom-radio'>
                 <input type='radio' name='road-network' id='road-network-new' value='new' onChange={this.onFieldChange.bind(null, 'roadNetworkSource')} checked={this.state.data.roadNetworkSource === 'new'}/>
                 <span className='form__option__text'>{t('Upload new')}</span>
+                <span className='form__option__ui'></span>
+              </label>
+              <label className='form__option form__option--inline form__option--custom-radio disabled'>
+                <input type='radio' name='road-network' id='road-network-osm' value='osm' onChange={this.onFieldChange.bind(null, 'roadNetworkSource')} checked={this.state.data.roadNetworkSource === 'osm'}/>
+                <span className='form__option__text'>{t('OSM data')}</span>
                 <span className='form__option__ui'></span>
               </label>
             </div>
